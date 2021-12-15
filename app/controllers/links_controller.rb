f1990e1070c039f57c1cf7bbbe376494bac1c70b
mohamedvall ibrahim <forvall.me@gmail.com>
2021-12-14 23:17:23 -1200

    add edit post for moderator

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,7 @@
 class LinksController < ApplicationController
   before_action :authenticate_user!
+  before_action :authenticate_moderator!, only: %i[edit update]
+  before_action :set_link, only: %i[edit update]
 
   def new
   end
@@ -21,7 +23,21 @@ class LinksController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @link.update(link_params)
+      redirect_to @link.post
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def authenticate_moderator!
+    redirect_to root_path, notice: t('not_authorized') unless current_user&.moderator?
+  end
 
   def link_params
     params.require(:link).permit(:url)
@@ -29,5 +45,9 @@ class LinksController < ApplicationController
 
   def post_params
     params.require(:link).permit(:community_id, :title)
+  end
+
+  def set_link
+    @link = Link.find(params[:id])
   end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,7 @@
 class TopicsController < ApplicationController
   before_action :authenticate_user!
+  before_action :authenticate_moderator!, only: %i[edit update]
+  before_action :set_topic, only: %i[edit update]
 
   def new
   end
@@ -21,7 +23,25 @@ class TopicsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @topic.update(topic_params)
+      redirect_to @topic.post
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  def authenticate_moderator!
+    redirect_to root_path, notice: t('not_authorized') unless current_user&.moderator?
+  end
 
   def topic_params
     params.require(:topic).permit(:text)
